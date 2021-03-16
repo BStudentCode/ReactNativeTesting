ca1a91777f981d375bf98e646ad0897f337da92a
B <hennyhexservers@gmail.com>
2021-03-16 23:31:35 +0000

    start project
    Signed-off-by: B <hennyhexservers@gmail.com>

--- App.js
+++ App.js
@@ -1,21 +1,28 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { StyleSheet, Text, View, SafeAreaView } from "react-native";
 
 export default function App() {
+  const handlePress = () => console.log("Text pressed");
+
+  console.log("app executed");
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <SafeAreaView style={styles.container}>
+      <Text numberOfLines={1} onPress={handlePress}>
+        Hello World! Hello World! Hello World! Hello World! Hello World! Hello
+        World! Hello World! Hello World! Hello World! Hello World! Hello World!
+        Hello World! Hello World! Hello World! Hello World! Hello World! Hello
+        World! Hello World! Hello World! Hello World! Hello World!
+      </Text>
       <StatusBar style="auto" />
-    </View>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "dodgerblue",
   },
 });

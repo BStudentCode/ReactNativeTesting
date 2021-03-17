b93085c4c84d1a57687044fb848594b368ebcb91
B <hennyhexservers@gmail.com>
2021-03-17 16:55:30 +0000

    Padding/Margin
    Signed-off-by: B <hennyhexservers@gmail.com>

--- App.js
+++ App.js
@@ -5,12 +5,19 @@ import ViewImageScreen from "./app/screens/ViewImageScreen";
 import WelcomeScreen from "./app/screens/WelcomeScreen";
 
 
-//ANDROID ONLY ACCEPTS "ELEVATION" FOR SHADOW
+//PADDING - SPACE INSIDE OBJECT
+//MARGIN - SPACE AROUND OBJECT
 export default function App() {
   return (
     <View style={{flex: 1, justifyContent: "center", alignItems: "center"}}>
-      <View style={{backgroundColor: "dodgerblue", width: 100, height: 100, shadowColor: "grey", shadowOffset: {width: 10, height: 10}, shadowOpacity: 1, shadowRadius: 10, elevation: 10}}>
+      <View style={{backgroundColor: "dodgerblue", width: 100, height: 100, padding: 20, paddingHorizontal: 10, paddingLeft: 30}}>
+        <View style={{
+          backgroundColor: "gold",
+          width: 50,
+          height: 50
+        }}></View>
       </View>
+      <View style={{backgroundColor: "tomato", width: 100, height: 100, margin: 20}}></View>
     </View>
   );
 }

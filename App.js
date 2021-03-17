197b0a5e4a2125cee91c403016d935ffd43159ca
B <hennyhexservers@gmail.com>
2021-03-17 17:11:44 +0000

    Icons
    Signed-off-by: B <hennyhexservers@gmail.com>

--- App.js
+++ App.js
@@ -4,20 +4,13 @@ import { Text, View } from "react-native";
 import ViewImageScreen from "./app/screens/ViewImageScreen";
 import WelcomeScreen from "./app/screens/WelcomeScreen";
 import AppText from './app/components/AppText';
+import {MaterialCommunityIcons} from '@expo/vector-icons';
 
 export default function App() {
   return (
     <View style={{flex: 1, justifyContent: "center", alignItems: "center"}}>
-      <AppText style={{
-        fontSize: 30, 
-        fontStyle: "italic", 
-        fontWeight: "600", 
-        color: "tomato", 
-        textTransform: "capitalize",
-        textAlign: "center",
-        lineHeight: 40
-        }}>Hello! I love React Native</AppText>
-
+      <AppText>Hello! I love React Native</AppText>
+    <MaterialCommunityIcons name="email" size={200} color="dodgerblue" />
     </View>
   );
 }

491c693ca94ff25de6b9a20a474b2fdce5b2c4a7
B <hennyhexservers@gmail.com>
2021-03-17 01:26:34 +0000

    dimensions and orientation changes
    Signed-off-by: B <hennyhexservers@gmail.com>

---
+++ AppNOTES.js
@@ -0,0 +1,78 @@
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import {
+  StyleSheet,
+  Text,
+  View,
+  Image,
+  TouchableHighlight,
+  SafeAreaView,
+  Button,
+  Alert,
+  Platform,
+} from "react-native";
+import {
+  useDimensions,
+  useDeviceOrientation,
+} from "@react-native-community/hooks";
+
+//<Image source={require("./assets/icon.png")} /> for local images
+/* <Image
+        source={{
+          width: 200,
+          height: 300,
+          uri: "https://picsum.photos/200/300",
+        }}
+      />
+      for network images, must define uri, w/h */
+
+//DOCS, IMAGE https://reactnative.dev/docs/image
+
+//IMAGES DONT HAVE ONCLICK, MUST USE TOUCHABLE TouWF, TouchableOpacity, TouchableHighlight
+
+//TouchableNativeFeedback only works on Android
+
+//CTRL SPACE TO SEE PROPERTIES OF OBJECT
+
+//PROMPT ALERTS ONLY WORK ON iOS
+
+//WHEN IMPORTING ARRAY OF STYLES, OBJECT ON THE RIGHT OVERWRITES PROPS OF OBJECT ON THE LEFT
+
+//import react-native-community/hooks for orientation detection useDimensions() useDeviceOrientation()
+
+export default function App() {
+  const { landscape } = useDeviceOrientation(); //get Landscape node from Orientation object
+
+  return (
+    <SafeAreaView style={[styles.container, containerStyle]}>
+      <Button
+        title="Click Me"
+        color="cyan"
+        onPress={() =>
+          Alert.alert("My title", "My message", [
+            { text: "YES! 😀", onPress: () => console.log("Pressed YES") },
+            { text: "NO! 😣", onPress: () => console.log("Pressed NO!") },
+          ])
+        }
+      />
+
+      <Button
+        title="Prompt Me"
+        color="aqua"
+        onPress={() =>
+          Alert.prompt("My title", "My message", (text) => console.log(text))
+        }
+      />
+    </SafeAreaView>
+  );
+}
+
+const containerStyle = { backgroundColor: "orange" };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    paddingTop: Platform.OS === "android" ? 20 : 0, //IF PLATFORM OS IS ANDROID ADD 20 PADDING TO TOP ELSE ADD 0 PADDING
+  }, //could use StatusBar.CurrentHeight but SafeAreaView now works on Android
+});

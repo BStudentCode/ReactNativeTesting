5897d8d8c8624332dac569aa997eb48d23f90c7a
B <hennyhexservers@gmail.com>
2021-03-17 00:30:45 +0000

    Touchables
    Signed-off-by: B <hennyhexservers@gmail.com>

--- App.js
+++ App.js
@@ -1,6 +1,13 @@
 import { StatusBar } from "expo-status-bar";
 import React from "react";
-import { StyleSheet, Text, View, Image, SafeAreaView } from "react-native";
+import {
+  StyleSheet,
+  Text,
+  View,
+  Image,
+  TouchableHighlight,
+  SafeAreaView,
+} from "react-native";
 
 //<Image source={require("./assets/icon.png")} /> for local images
 /* <Image
@@ -13,18 +20,22 @@ import { StyleSheet, Text, View, Image, SafeAreaView } from "react-native";
       for network images, must define uri, w/h */
 
 //DOCS, IMAGE https://reactnative.dev/docs/image
+//IMAGES DONT HAVE ONCLICK, MUST USE TOUCHABLE TouWF, TouchableOpacity, TouchableHighlight
+//TouchableNativeFeedback only works on Android
 
 export default function App() {
   return (
     <SafeAreaView style={styles.container}>
       <Text>Hello World!</Text>
-      <Image
-        source={{
-          width: 200,
-          height: 300,
-          uri: "https://picsum.photos/200/300",
-        }}
-      />
+      <TouchableHighlight onPress={() => console.log("image click out")}>
+        <Image
+          source={{
+            width: 200,
+            height: 300,
+            uri: "https://picsum.photos/200/300",
+          }}
+        />
+      </TouchableHighlight>
       <StatusBar style="auto" />
     </SafeAreaView>
   );

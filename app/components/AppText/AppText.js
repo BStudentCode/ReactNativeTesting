441660be46c4609136a58558afa7c134239bd988
B <hennyhexservers@gmail.com>
2021-03-20 21:55:52 +0000

    centralize styles & colors
    Signed-off-by: B <hennyhexservers@gmail.com>

--- app/components/AppText/AppText.js
+++ app/components/AppText/AppText.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import { Text } from 'react-native';
 
-import styles from './styles'
+import defaultStyles from '../../config/defaultStyles'
 //can store styles in separate file
 
 //props.children, destructured
 //Another example of overriding a style using array
 function AppText({children, style}) {
     return (
-        <Text style={[styles.text, style]}>{children}</Text>
+        <Text style={[defaultStyles.text, style]}>{children}</Text>
     );
 }
 
